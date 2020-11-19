750895d57f41910ae3830ce4e2f0a6571fc3e64b
Max Woodworth <max10woodworth@gmail.com>
2020-11-19 09:31:20 -0600

    #3 departments
    Adds listing manager of each department with corresponding manager/dept information

--- data_analysis_queries.sql
+++ data_analysis_queries.sql
@@ -2,8 +2,17 @@
 select employees.emp_no, employees.last_name, employees.first_name, employees.sex, employees.salary
 from employees
     INNER JOIN salaries
-    ON (employees.emp_no=salaries.emp_no)
+        ON (employees.emp_no=salaries.emp_no)
 
---List first name, last name, and hire date for employees who were hired in 1986
+--2.) List first name, last name, and hire date for employees who were hired in 1986
 select first_name, last_name, hire_date from employees
 where hire_date >= '1986-01-01' and hire date <= '1986-12-31';
+
+--3.) List the manager of each department with the following information: department number, department name, the manager's employee number, last name, and first name.
+select employees.emp_no, employees.last_name, employees.first_name, dept_manager.dept_no, department.dept_name
+from dept_manager
+    INNER JOIN employees on
+    dept_manager.emp_no=employees.emp_no
+    INNER JOIN departments on
+    dept_manager.dept_no=departments.dept_no
+
